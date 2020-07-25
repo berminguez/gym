80e128f8749485305d781564d914e63c775561aa
berminguez <berminguez@gmail.com>
2020-07-25 20:04:55 +0200

    Commit cambios dia 1

---
+++ public_html/js/sportin.js
@@ -0,0 +1,21 @@
+$(document).ready(function(){
+
+    $('.owl-carousel').owlCarousel({
+        center: true,
+        items:1,
+        loop:true,
+        margin:0,
+        nav: true,
+        dots: false,
+        padding: 0,
+        mergeFit: false,
+        responsive:{
+            600:{
+                items:1
+            }
+        }
+    });
+
+  });
+
+  
--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,16 @@ module.exports = {
     extend: {
       colors: {
         sportinred: '#d92619',
+        sportingray: '#303030'
       },
+      fontFamily: {
+        roboto: ['Roboto', 'sans-serif'],
+        fira: ['Fira Sans', 'sans-serif'],
+        raleway: ['Raleway', 'serif']
+      },
+      container: {
+        padding: '2rem',
+      }
     },
   },
   variants: {},
